bc9f29c90f762c8a2aec17cb6b5df8652e8619c8
Bri <brianjcarroll0@gmail.com>
2024-04-16 12:09:07 -0400

    new fonts and styles

---
+++ src/components/icons/HorizontalRule.tsx
@@ -0,0 +1,13 @@
+export default function HorizontalRule() {
+  return (
+    <svg
+      // width="1012"
+      height="29"
+      viewBox="0 0 1012 29"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="10" y="14.1895" width="992" height="1" fill="#292929" />
+    </svg>
+  );
+}
