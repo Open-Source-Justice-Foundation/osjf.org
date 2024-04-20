fea3d38e14c70c228986e20a9890b572b3175e84
BitcoinBrief <115094037+BitcoinBrief@users.noreply.github.com>
2024-04-20 07:33:35 -0400

    Update Projects.tsx
    Copy editing, changed projects header title

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,7 +3,7 @@ import SectionTitle from "../shared/SectionTitle";
 export default function Projects() {
   return (
     <div className="mt-20 p-2.5">
-      <SectionTitle>Projects</SectionTitle>
+      <SectionTitle>Supporting Projects that Spread Liberty and Prosperity</SectionTitle>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div className="flex flex-col gap-4">
           <Fundsolvr />
@@ -11,9 +11,10 @@ export default function Projects() {
             Fundsolvr
           </h4>
           <p className="font-hubot text-lg text-heading">
-            Fundsolvr is a peer-to-peer crowdfunding marketplace that aggregates funding for open-source
-            projects and turns issues into bounties paid directly to builders and project maintainers, 
-            enabling open-source contributors to benefit from a value-for-value economy.
+            Fundsolvr is a peer-to-peer crowdfunding marketplace for open-source
+            projects that turns issues into bounties paid directly to builders and project maintainers, 
+            enabling open-source contributors to benefit from a value-for-value economy. And Fundsolvr's integrated 
+            dispute resolution tools provide assurances that further increase market efficiencies.
           </p>
           <div className="flex flex-row gap-4">
             <Duck />
@@ -26,8 +27,8 @@ export default function Projects() {
             Keystache
           </h4>
           <p className="font-hubot text-lg text-heading">
-            Keystache is core infrastructure for the distributed communication
-            protocol NOSTR that enables users to store their private keys in a
+            Keystache is core infrastructure for the censorship-resistant, distributed 
+            communication protocol NOSTR that enables users to store their cryptographic keys in a
             single desktop app, allowing a seamless OAUTH-style login experience
             across NOSTR-applications.
           </p>
